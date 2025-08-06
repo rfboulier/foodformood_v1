862a4c806f5d84d43f42f7c8ef235d3c83a696a9
rfboulier <pro.boulier.romane@gmail.com>
2025-08-06 17:24:19 +0200

    - commit 6/8/25

--- src/Controller/RecommandationController.php
+++ src/Controller/RecommandationController.php
@@ -3,79 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\Recommandation;
-use App\Form\RecommandationType;
 use App\Repository\RecommandationRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/recommandation')]
 final class RecommandationController extends AbstractController
 {
-    #[Route(name: 'app_recommandation_index', methods: ['GET'])]
+    #[Route('/list', name: 'list_recommandation')]
     public function index(RecommandationRepository $recommandationRepository): Response
     {
-        return $this->render('recommandation/index.html.twig', [
-            'recommandations' => $recommandationRepository->findAll(),
+        $reco = $recommandationRepository->findAll();
+
+        return $this->render('recommandation/list.html.twig', [
+            'recos' => $reco,
         ]);
-    }
-
-    #[Route('/new', name: 'app_recommandation_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $recommandation = new Recommandation();
-        $form = $this->createForm(RecommandationType::class, $recommandation);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($recommandation);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_recommandation_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('recommandation/new.html.twig', [
-            'recommandation' => $recommandation,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_recommandation_show', methods: ['GET'])]
-    public function show(Recommandation $recommandation): Response
-    {
-        return $this->render('recommandation/show.html.twig', [
-            'recommandation' => $recommandation,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_recommandation_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Recommandation $recommandation, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(RecommandationType::class, $recommandation);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_recommandation_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('recommandation/edit.html.twig', [
-            'recommandation' => $recommandation,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_recommandation_delete', methods: ['POST'])]
-    public function delete(Request $request, Recommandation $recommandation, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$recommandation->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($recommandation);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_recommandation_index', [], Response::HTTP_SEE_OTHER);
     }
 }
